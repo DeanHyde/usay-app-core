2bf6c77f99b12e02018e225dd91b01ac708f12bc
Dean Hyde <dean.hyde@usaycompare.com>
2018-05-24 09:01:53 +0100

    Refactored

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
+const authURL = process.env.APP_AUTH_MANAGER_NODE_URL;
+
 module.exports.checkAuth = function(req, res, next) {
 
   // If we have a session and we're authenticated, carry on...
   if (req.session !== undefined && req.session.authenticated)
     return next();
   // Otherwise, we redirect off to our authentication manager
-  res.redirect(process.env.APP_AUTH_MANAGER_NODE_URL + '/login?redirect='+process.env.APP_URL);
+  res.redirect(authURL + '/login?redirect='+process.env.APP_URL);
 }
 
-module.exports.setCookie = function(req, res) {
+module.exports.setup = function(app) {
+  // Setup setCookie route
+  app.get('/setCookie', (req, res) => setCookie(req, res));
+  // Setup logout route
+  app.get('/logout', (req, res) => res.redirect(authURL+'/logout'));
+  // Setup session
+  app.use(usaySession());
+}
+
+function setCookie(req, res) {
 
   // If we have a session ID in our query params, use that as our new session ID and redirect to homepage
   if (req.query.cookieID !== undefined) {
@@ -22,10 +33,9 @@ module.exports.setCookie = function(req, res) {
   } else {
     console.log('failed setting session');
   }
-
 }
 
-module.exports.session = function() {
+function usaySession() {
   return session({
     store: new RedisStore({host: 'redis', port: 6379}),
     secret: 'keyboard cat',
