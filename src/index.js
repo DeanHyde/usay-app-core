7972d2156a47e402c2318bb55e377b53aaff2c5e
Dean Hyde <dean.hyde@usaycompare.com>
2018-05-31 10:21:34 +0100

    Moved some more stuff into global package

--- src/index.js
+++ src/index.js
@@ -1,26 +1,57 @@
 const session = require('express-session');
+const exphbs  = require('express-handlebars');
 const RedisStore = require('connect-redis')(session);
 
 const authURL = process.env.APP_AUTH_MANAGER_URL;
 
-module.exports.checkAuth = function(req, res, next) {
+const handlebarsConfig = {
+  extname: '.hbs',
+  layoutsDir: 'src/resources/views/layouts',
+  defaultLayout: 'main',
+  helpers: {
+    jsonToString: function(json) { return JSON.stringify(json, null, 4); },
+    hasRole: function(role, options) {
+      return (options.hash.roles.find(o => o.name === role) !== undefined) ? options.fn(this) : options.inverse(this)
+    }
+  }
+};
 
+// Checks if a user is authenticated based on their session and redirects them to login if not
+module.exports.checkAuth = function(req, res, next) {
   // If we have a session and we're authenticated, carry on...
   if (req.session !== undefined && req.session.authenticated)
     return next();
   // Otherwise, we redirect off to our authentication manager
   res.redirect(authURL + '/login?redirect=//'+req.headers.host);
-}
+};
 
-module.exports.setup = function(app) {
-  // Setup setCookie route
-  app.get('/setCookie', (req, res) => setCookie(req, res));
-  // Setup logout route
-  app.get('/logout', (req, res) => res.redirect(authURL+'/logout'));
+// Bootstraps our node application
+module.exports.setup = function(app, authManager=false) {
+  // Don't setup the following routes for the auth manager
+  if (!authManager) {
+    // Setup setCookie route
+    app.get('/setCookie', (req, res) => setCookie(req, res));
+    // Setup logout route
+    app.get('/logout', (req, res) => res.redirect(authURL + '/logout'));
+  }
   // Setup session
   app.use(usaySession());
-}
+  // Enable handlebars for templating views
+  let hbs = exphbs.create(handlebarsConfig);
+  app.engine('.hbs', hbs.engine);
+  app.set('view engine', '.hbs');
+  app.set('views', 'src/resources/views');
+  // Add session and ENV data to all view's data
+  app.use(function (req, res, next) {
+    res.locals = {
+      session: req.session,
+      env: process.env
+    };
+    next();
+  });
+};
 
+// Sets a cookie on the requesting micro-service frontend based on the cookie data from our auth server
 function setCookie(req, res) {
 
   // If we have a session ID in our query params, use that as our new session ID and redirect to homepage
@@ -35,6 +66,7 @@ function setCookie(req, res) {
   }
 }
 
+// Bootstrap our session
 function usaySession() {
   return session({
     store: new RedisStore({host: 'redis', port: 6379}),
